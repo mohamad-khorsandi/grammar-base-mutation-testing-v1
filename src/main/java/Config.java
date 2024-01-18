7d98f23421aba11e164e60fdf30aecd25772f3cf
mohammad <9m.kh557@gmai.com>
2024-01-18 13:33:30 +0330

    mohammad3

---
+++ src/main/java/Config.java
@@ -0,0 +1,11 @@
+import java.util.Set;
+
+public class Config {
+
+    public Set<String> mutableSegments = Set.of(
+            "SimpleName",//todo
+            "IntegerLiteralExpr",
+            "BinaryExpr",
+            "BooleanLiteralExpr"
+    );
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,20 @@
 import ast_tree.AstTreeGenerator;
+import ast_tree.SimpleNode;
 import code_generation.RandomCodeGenerator;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.expr.BinaryExpr;
 import mutation.Operator;
 
-public class Main {
-    public static void main(String[] args) {
-        AstTreeGenerator
-        // parse tree
 
-        // choose part
+import java.io.FileNotFoundException;
+import java.util.*;
+
+
+public class Main {
+    static Config config = new Config();
+    static Random random = new Random();
+    public static void main(String[] args) throws FileNotFoundException {
+        Node treeParent = AstTreeGenerator.getTreeParent();
 
         // generate new code for chosen part
 
@@ -15,4 +22,41 @@ public class Main {
 
         System.out.println();
     }
+    static Node chooseTreeSegment(Node treeParent) {
+        Node funcParent = BFSFirst(treeParent, "MethodDeclaration")
+                .orElseThrow(() -> new RuntimeException("there is no method to test"));
+
+        ArrayList<Node> availMutableSegments = BFSAll(funcParent, config.mutableSegments);
+
+        int randomIndex = random.nextInt(availMutableSegments.size());
+        return availMutableSegments.get(randomIndex);
+    }
+
+    static Optional<Node> BFSFirst(Node cur, String type) {
+        LinkedList<Node> queue = new LinkedList<>();
+        queue.add(cur);
+
+        while (!queue.isEmpty()) {
+            cur = queue.poll();
+            if (cur.getClass().getSimpleName().equals(type))
+                return Optional.of(cur);
+            queue.addAll(cur.getChildNodes());
+        }
+        return Optional.empty();
+    }
+
+    static ArrayList<Node> BFSAll(Node cur, Set<String> types) {
+        ArrayList<Node> result = new ArrayList<>();
+        LinkedList<Node> queue = new LinkedList<>();
+        queue.add(cur);
+
+        while (!queue.isEmpty()) {
+            cur = queue.poll();
+            if (types.contains(cur.getClass().getSimpleName()))
+                result.add(cur);
+            queue.addAll(cur.getChildNodes());
+        }
+        return result;
+    }
 }
+
